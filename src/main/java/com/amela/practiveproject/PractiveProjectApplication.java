38db7c26f59982cfd5aac63c1d404b4810cd6792
Nguyen Khac Manh <manhnguyenkhac3496@gmail.com>
2021-11-08 15:47:50 +0700

    The empty project

---
+++ src/main/java/com/amela/practiveproject/PractiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amela.practiveproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiveProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PractiveProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amela/practiveproject/PractiveProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amela.practiveproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiveProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
